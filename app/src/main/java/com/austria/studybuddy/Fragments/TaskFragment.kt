96b3911728ac82608260d3a993192f4e0bd27b03
lmsablot <loreleen.sablot@navagis.com>
2023-08-12 16:06:08 +0800

    Added a new dialog

--- app/src/main/java/com/austria/studybuddy/Fragments/TaskFragment.kt
+++ app/src/main/java/com/austria/studybuddy/Fragments/TaskFragment.kt
@@ -4,15 +4,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.austria.studybuddy.NewTaskDialog
+import com.austria.studybuddy.OnTaskAddListener
 import com.austria.studybuddy.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class TaskFragment : Fragment() {
+class TaskFragment : Fragment(), OnTaskAddListener {
+
+    private var newTaskDialog: NewTaskDialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_task, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabNewTask()
+    }
+
+    private fun fabNewTask() {
+        val fabAddTask: FloatingActionButton? = activity?.findViewById(R.id.fabNewTask2)
+        fabAddTask?.setOnClickListener {
+            newTaskDialog = activity?.let { it1 -> NewTaskDialog(it1, this) }?.apply {
+                show()
+            }
+        }
+    }
+
+    override fun onTaskAdded() {
+        TODO("Not yet implemented")
     }
 }
 
--- app/src/main/java/com/austria/studybuddy/NewTaskDialog.kt
+++ app/src/main/java/com/austria/studybuddy/NewTaskDialog.kt
@@ -2,19 +2,21 @@ package com.austria.studybuddy
 
 import android.app.AlertDialog
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.austria.studybuddy.databinding.ActivityNewTaskBinding
+import android.view.LayoutInflater
+import com.austria.studybuddy.databinding.DialogNewTaskBinding
 
 class NewTaskDialog(
     context: Context,
     private val listener: OnTaskAddListener? = null
 ): AlertDialog(context) {
 
+    private var binding: DialogNewTaskBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        binding = DialogNewTaskBinding.inflate(LayoutInflater.from(context))
+        setContentView(binding!!.root)
     }
 
 }
